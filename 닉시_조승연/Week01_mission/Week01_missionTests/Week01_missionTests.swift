cfa8570c72c206a0294f2420fb28f47f185c86ae
seung yeon <137742302+moanuna@users.noreply.github.com>
2024-09-29 14:59:37 +0900

    feat : 1주차 미션

---
+++ 닉시_조승연/Week01_mission/Week01_missionTests/Week01_missionTests.swift
@@ -0,0 +1,17 @@
+//
+//  Week01_missionTests.swift
+//  Week01_missionTests
+//
+//  Created by 조승연 on 9/29/24.
+//
+
+import Testing
+@testable import Week01_mission
+
+struct Week01_missionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
