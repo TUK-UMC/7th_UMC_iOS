704f4aa83675effc0b9742822d1e1016e8442585
seung yeon <137742302+moanuna@users.noreply.github.com>
2024-10-10 20:16:12 +0900

    feat: Week02 Study
    TableView로 주사위 1~6까지 나타내기

---
+++ 닉시_조승연/Week03_Study2/Week03_Study2Tests/Week03_Study2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Week03_Study2Tests.swift
+//  Week03_Study2Tests
+//
+//  Created by 조승연 on 10/10/24.
+//
+
+import Testing
+@testable import Week03_Study2
+
+struct Week03_Study2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
