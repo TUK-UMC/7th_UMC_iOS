02f61aba552ac4199b9fa78ba12c85b4a731e769
Kim Ui Taek <kut7728@hanyang.ac.kr>
2024-11-23 23:01:12 +0900

    week08_study done

---
+++ 쿠트_김의택/week08_study/week08_study/Model/FileResponseModel.swift
@@ -0,0 +1,14 @@
+//
+//  FileResponseModel.swift
+//  week08_study
+//
+//  Created by nelime on 11/23/24.
+//
+
+import Foundation
+
+struct FileResponseModel: Codable {
+    let originalname: String
+    let filename: String
+    let location: String
+}
